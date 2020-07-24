1daae1632e2a8463bc38b2380e0b46296866bfd2
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2020-07-24 11:03:40 -0500

    basic opening of ipynb named nubook

--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,64 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "nubook" is now active!');
+	// // Use the console to output diagnostic information (console.log) and errors (console.error)
+	// // This line of code will only be executed once when your extension is activated
+	// console.log('Congratulations, your extension "nubook" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('nubook.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// // The command has been defined in the package.json file
+	// // Now provide the implementation of the command with registerCommand
+	// // The commandId parameter must match the command field in package.json
+	// let disposable = vscode.commands.registerCommand('nubook.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from NuShell Extension!');
-	});
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from NuShell Extension!');
+	// });
 
-	context.subscriptions.push(disposable);
+	// context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.notebook.registerNotebookContentProvider(
+			"nushell-nubook", new NuShellNotebookProvider()
+		)
+	);
+}
+
+class NuShellNotebookProvider implements vscode.NotebookContentProvider {
+	async openNotebook(uri: vscode.Uri): Promise<vscode.NotebookData> {
+		const content = JSON.parse((await vscode.workspace.fs.readFile(uri)).toString());
+		return {
+			languages: [],
+			metadata: { custom: content.metadata },
+			cells: content.cells.map((cell: any) => {
+				if (cell.cell_type === 'markdown') {
+					return {
+						cellKind: vscode.CellKind.Markdown,
+						source: cell.source,
+						language: 'markdown',
+						outputs: [],
+						metadata: {}
+					};
+				} else if (cell.cell_type === 'code') {
+					return {
+						cellKind: vscode.CellKind.Code,
+						source: cell.source,
+						language: content.metadata?.language_info?.name || 'python',
+						outputs: [/* not implemented */],
+						metadata: {}
+					};
+				} else {
+					console.error('Unexpected cell:', cell);
+				}
+			})
+		};
+	}
+
+	// dummy implementations
+	onDidChangeNotebook = new vscode.EventEmitter<vscode.NotebookDocumentEditEvent>().event;
+	async resolveNotebook(): Promise<void> { }
+	async saveNotebook(): Promise<void> { }
+	async saveNotebookAs(): Promise<void> { }
+	async backupNotebook(): Promise<vscode.NotebookDocumentBackup> { return { id: '', delete: () => {}};}	
 }
 
 // this method is called when your extension is deactivated
