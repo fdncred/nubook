7abe06c47651aff74ae0d174359b9f961d320ff1
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2020-07-24 10:01:13 -0500

    simple updates

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from nushell_ext!');
+		vscode.window.showInformationMessage('Hello World from NuShell Extension!');
 	});
 
 	context.subscriptions.push(disposable);
